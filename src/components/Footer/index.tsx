9c787078d3e1818c19775608e013b1dd0f1a3f69
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-12 16:11:37 -0300

    Prepare project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { style } from "./styles";
+
+export default function Footer(){
+    return(
+        <View>
+            <Text>Footer</Text>
+        </View>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { style } from "./styles";
+
+export default function Header(){
+    return(
+        <View>
+            <Text>Header</Text>
+        </View>
+    );
+}
